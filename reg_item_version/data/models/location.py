33bb80a2c5e29a180927c59b58c89cc7fa0768f7
Арсений <94627349+Arz2212@users.noreply.github.com>
2022-04-13 12:36:08 +0300

    Add files via upload

---
+++ reg_item_version/data/models/location.py
@@ -0,0 +1,10 @@
+import sqlalchemy
+from ..db_session import SqlAlchemyBase
+
+
+class Location(SqlAlchemyBase):
+    __tablename__ = 'locations'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    level = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
---
+++ reg_item_version/main.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template
+from data import db_session
+from werkzeug.utils import redirect
+from data.models.player import Player
+from data.models.items import Items
+from data.forms import *
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+
+db_session.global_init('db/game_database.db')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3yzwyB8X88GaxcWDLkmFXG05GC0brVLKCTJFtr1'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(player_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(Player).get(player_id)
+
+
+@app.route('/')
+def main_page():
+    return render_template('basic_template.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = NewPlayerForm()
+    if form.validate_on_submit():
+        if form.password.data == form.password_again.data:
+            return render_template('registration_item_page.html', heading='Регистрация',
+                                   form=form,
+                                   message='Пароли не совпадают')
+        db_sess = db_session.create_session()
+        if db_sess.query(Player).filter(Player.email == form.email.data).first():
+            return render_template('registration_item_page.html', heading='Регистрация',
+                                   form=form,
+                                   message='Пользователь с такой почтой уже есть')
+        if form.player_class.data == 1:
+            health = 40
+        else:
+            health = 30
+        player = Player(
+            email=form.email.data,
+            name=form.name.data,
+            surname=form.surname.data,
+            nickname=form.nickname.data,
+            inventory=None,
+            equipped=None,
+            money=0,
+            location='square/0/free',
+            player_class=form.player_class.data,
+            hp=health,
+            level=1)
+        player.set_password(form.password.data)
+        db_sess.add(player)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('registration_item_page.html', heading='Регистрация',
+                           form=form)
+
+@app.route('/register_item', methods=['GET', 'POST'])
+def f():
+    itm = NewItem()
+    if itm.validate_on_submit():
+        db_sess = db_session.create_session()
+        itm = Items(
+            name=itm.name,
+            protection=itm.difens,
+            attack=itm.attac,
+            class_required=itm.classs,
+            cost=itm.costs,
+            item_type=itm.data
+        )
+        db_sess.add(itm)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('registration_item_page.html', heading='Регистрация',
+                           form=itm)
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
