433ec50439e973175608f06f26fd21a9cd37f6c8
Юлия <94623995+candleend@users.noreply.github.com>
2022-04-15 10:57:16 +0300

    Update main.py
    Добавление кнопки "удалить" в таблицу с предметами и врагами

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from os import abort
+
+from flask import Flask, render_template, request
 from data import db_session
 from werkzeug.utils import redirect
 from data.models.player import Player
@@ -9,8 +11,6 @@ from data.models.items import Items
 from data.models.item_type import ItemType
 from data.forms import *
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from os import abort
-
 
 db_session.global_init('db/game_database.db')
 app = Flask(__name__)
@@ -98,11 +98,99 @@ def message(text):
         return render_template('message_page.html', title='Внимание!', message=t)
 
 
+@app.route('/items_table', methods=['GET', 'POST'])
+def items_table():
+    db_sess = db_session.create_session()
+    flag = current_user in db_sess.query(Player).filter(Player.id.in_([1, 2, 3]))
+    i = db_sess.query(Items)
+    items = []
+    for thing in i:
+        req_class = db_sess.query(PlayerClass).filter(PlayerClass.id == thing.class_required).first().name
+        item_type = db_sess.query(ItemType).filter(ItemType.id == thing.item_type).first().name
+        mini = [thing.id, thing.name, thing.rarity, item_type, thing.protection, thing.attack, req_class, thing.cost]
+        items.append(mini)
+    return render_template('items_page.html', heading='Просмотр вещей', items=items, access=flag)
+
+
+@app.route('/enemy_table', methods=['GET', 'POST'])
+def enemy_table():
+    db_sess = db_session.create_session()
+    flag = current_user in db_sess.query(Player).filter(Player.id.in_([1, 2, 3]))
+    i = db_sess.query(Enemy)
+    enemies = []
+    for antagonist in i:
+        location = db_sess.query(Location).filter(Location.id == antagonist.location).first().name
+        mini = [antagonist.id, antagonist.name, location, antagonist.min_level]
+        enemies.append(mini)
+    return render_template('enemy_page.html', heading='Просмотр врагов', enemies=enemies, access=flag)
+
+
+@app.route('/enemy_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def enemy_delete(id):
+    db_sess = db_session.create_session()
+    enemy = db_sess.query(Enemy).filter(Enemy.id == id).first()
+    if enemy:
+        db_sess.delete(enemy)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/enemy_table')
+
+
+@app.route('/items_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def items_delete(id):
+    db_sess = db_session.create_session()
+    items = db_sess.query(Items).filter(Items.id == id).first()
+    if items:
+        db_sess.delete(items)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/items_table')
+
+
+@app.route('/new_enemy', methods=['GET', 'POST'])
+@login_required
+def add_enemy():
+    form = NewEnemyForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        monster = Enemy(name=form.name.data,
+                        location=form.location.data,
+                        min_level=int(form.min_level.data))
+        db_sess.add(monster)
+        db_sess.commit()
+        return redirect('/enemy_table')
+    return render_template('new_enemy_page.html', title='Добавление врага',
+                           form=form)
+
+
+@app.route('/new_item', methods=['GET', 'POST'])
+@login_required
+def add_item():
+    db_sess = db_session.create_session()
+    form = NewItemForm()
+    if form.validate_on_submit():
+        item = Items(name=form.name.data,
+                     rarity=form.rarity.data,
+                     item_type=form.item_type.data,
+                     protection=int(form.protection.data),
+                     attack=int(form.attack.data),
+                     class_required=form.class_required.data,
+                     cost=int(form.cost.data))
+        db_sess.add(item)
+        db_sess.commit()
+        return redirect('/items_table')
+    return render_template('new_item_page.html', heading='Новый предмет', form=form)
+
+
 @app.route('/new_enemy/<int:id>', methods=['GET', 'POST'])
 @login_required
-def new_enemy():
+def new_enemy(id):
     db_sess = db_session.create_session()
-    if current_user not in db_sess.query(Player).filter(Player.id.in_([1, 2, 3])).fetchall():
+    if current_user not in db_sess.query(Player).filter(Player.id.in_([0, 2, 3])).fetchall():
         return redirect('/message/low_access')
     form = NewEnemyForm()
     if request.method == "GET":
@@ -113,7 +201,7 @@ def new_enemy():
             form.location.data = enemy.location
             form.min_level.data = int(enemy.min_level)
         else:
-            abort(404)    
+            abort(404)
     if form.validate_on_submit():
         if db_sess.query(Location).filter(Location.id == form.location.data).first().level > int(form.min_level.data):
             return render_template('new_enemy_page.html', heading='Новый враг',
@@ -129,7 +217,7 @@ def new_enemy():
         db_sess.add(monster)
         db_sess.commit()
         return redirect('/enemy_table')
-    return render_template('new_enemy_page.html', heading='Новый враг', form=form)
+    return render_template('new_enemy_page.html', heading='Редактирование врага', form=form)
 
 
 @app.route('/new_item/<int:id>', methods=['GET', 'POST'])
@@ -174,35 +262,8 @@ def new_item(id):
                      cost=int(form.cost.data))
         db_sess.add(item)
         db_sess.commit()
-        return redirect('/items_table'')
+        return redirect('/items_table')
     return render_template('new_item_page.html', heading='Новый предмет', form=form)
-
-
-@app.route('/items_table', methods=['GET', 'POST'])
-def items_table():
-    db_sess = db_session.create_session()
-    flag = current_user in db_sess.query(Player).filter(Player.id.in_([1, 2, 3]))
-    i = db_sess.query(Items)
-    items = []
-    for thing in i:
-        req_class = db_sess.query(PlayerClass).filter(PlayerClass.id == thing.class_required).first().name
-        item_type = db_sess.query(ItemType).filter(ItemType.id == thing.item_type).first().name
-        mini = [thing.id, thing.name, thing.rarity, item_type, thing.protection, thing.attack, req_class, thing.cost]
-        items.append(mini)
-    return render_template('items_page.html', heading='Просмотр вещей', items=items, access=flag)
-
-
-@app.route('/enemy_table', methods=['GET', 'POST'])
-def enemy_table():
-    db_sess = db_session.create_session()
-    flag = current_user in db_sess.query(Player).filter(Player.id.in_([1, 2, 3]))
-    i = db_sess.query(Enemy)
-    enemies = []
-    for antagonist in i:
-        location = db_sess.query(Location).filter(Location.id == antagonist.location).first().name
-        mini = [antagonist.id, antagonist.name, location, antagonist.min_level]
-        enemies.append(mini)
-    return render_template('enemy_page.html', heading='Просмотр врагов', enemies=enemies, access=flag)
 
 
 if __name__ == '__main__':
